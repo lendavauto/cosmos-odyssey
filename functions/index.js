d9a447d7a321de1d700f39c0ab87ef552c7500fa
Andreas Aus <ausandreas@gmail.com>
2021-04-23 22:07:59 +0300

    Cors error fixed on production

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.customApi = functions.https.onRequest((req, res) => {
+ const cors = require('cors')({ origin: true });
+  cors(req, res, () => {
+    const axios = require('axios');
+    let endpoint =
+     'https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices';
+     axios
+       .get(endpoint)
+       .then((response) => {
+         res.json(response.data);
+       })
+       .catch((error) => {
+         res.json(error);
+       });
+  });
+});
--- src/components/Flights.js
+++ src/components/Flights.js
@@ -53,7 +53,7 @@ const FlightsWrapper = styled.div`
 
 const Flights = () => {
   const [{ offersLoading, apiData }, dispatch] = useStateValue();
-  const url = 'http://localhost:3000/TravelPrices';
+  const url = 'https://us-central1-cosmos-odyssey.cloudfunctions.net/customApi';
 
   const fetchData = async (url) => {
     dispatch({
